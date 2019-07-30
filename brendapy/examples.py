96eb7970defd93b43ce687b232226e287c8c0c12
Matthias Koenig <konigmatt@googlemail.com>
2019-07-30 10:03:41 +0200

    fixing pep8 issues

---
+++ brendapy/examples.py
@@ -0,0 +1,22 @@
+"""
+Examples of using brendapy
+"""
+from brendapy import BrendaParser, BrendaProtein
+
+
+def parse_proteins_for_ec():
+    """Parse the protein entries for a given EC number in BRENDA."""
+    brenda = BrendaParser()
+    ec = "1.1.1.1"
+    proteins = brenda.parse_proteins(ec)
+    print(f"{len(proteins)} proteins for EC {ec} in BRENDA")
+    print(f"Protein identifier: {proteins.keys()}")
+    print("-"*80)
+    for p in proteins.values():
+        if p.organism == "Homo sapiens":
+            print(p)
+    print("-" * 80)
+
+
+if __name__ == "__main__":
+    parse_proteins_for_ec()
--- brendapy/tests/test_brenda.py
+++ brendapy/tests/test_brenda.py
@@ -1,66 +1,69 @@
 
 import pytest
-from brendapy import parser
+from brendapy import BrendaParser, BrendaProtein
 from brendapy.settings import BRENDA_FILE
 
 
 def test_parsing():
-	brenda = parser.BRENDAparser(BRENDA_FILE)
-	assert brenda
-	assert "1.1.1.1" in brenda.ec_text
+    brenda = BrendaParser()
+    assert brenda
+    assert "1.1.1.1" in brenda.ec_text
+
+
+def test_parsing_from_file():
+    brenda = BrendaParser(brenda_file=BRENDA_FILE)
+    assert brenda
+    assert "1.1.1.1" in brenda.ec_text
 
 
 def test_protein():
-	""" Test the proteinBRENDA module """
-	brenda = parser.BRENDAparser(BRENDA_FILE)
+    """ Test the proteinBRENDA module """
+    brenda = BrendaParser()
 
-	ec = "1.1.1.1"
-	proteins = brenda.parse_proteins(ec)
-	assert proteins
-	assert len(proteins) == 167
+    ec = "1.1.1.1"
+    proteins = brenda.parse_proteins(ec)
+    assert proteins
+    assert len(proteins) == 167
 
 
 def test_protein_detail1():
-	brenda = parser.BRENDAparser(BRENDA_FILE)
-	ec = "1.1.1.1"
-	ec_str = brenda.ec_text[ec]
-	protein = parser.BRENDAProtein(ec=ec, id=1, ec_string=ec_str)
-	assert protein
-	assert 'liver' in protein.source_tissue
-	assert protein.organism == "Gallus gallus"
-
-	print(protein)
-	assert 44 in protein.references
-	assert len(protein.pubmed) == 1
+    brenda = BrendaParser()
+    ec = "1.1.1.1"
+    ec_str = brenda.ec_text[ec]
+    protein = BrendaProtein(ec=ec, id=1, ec_string=ec_str)
+    assert protein
+    assert 'liver' in protein.source_tissue
+    assert protein.organism == "Gallus gallus"
+    assert 44 in protein.references
+    assert len(protein.pubmed) == 1
 
 
 def test_protein_detail2():
-	brenda = parser.BRENDAparser(BRENDA_FILE)
-	ec = "1.1.1.1"
-	ec_str = brenda.ec_text[ec]
-	protein = parser.BRENDAProtein(ec=ec, id=4, ec_string=ec_str)
-	assert protein
-	print(protein)
-	assert 'liver' in protein.source_tissue
-	assert protein.organism == "Drosophila melanogaster"
+    brenda = BrendaParser()
+    ec = "1.1.1.1"
+    ec_str = brenda.ec_text[ec]
+    protein = BrendaProtein(ec=ec, id=4, ec_string=ec_str)
+    assert protein
+    assert protein.organism == "Drosophila melanogaster"
 
-	assert 8 in protein.references
-	assert len(protein.pubmed) == 7
+    assert 8 in protein.references
+    assert len(protein.pubmed) == 7
 
 
+'''
 def test_all_proteins():
-	brenda = parser.BRENDAparser(brenda_file)
-	for ec in brenda.keys():
-		print(ec)
-		proteins = brenda.parse_proteins(ec)
-
+    brenda = BrendaParser()
+    for ec in brenda.keys():
+        print(ec)
+        proteins = brenda.parse_proteins(ec)
 
 def test_info_dict():
-	brenda = parser.BRENDAparser(BRENDA_FILE)
-	ec = "1.1.1.2"
-	ec_str = brenda.ec_text[ec]
-	d = brenda.parse_info_dict(ec_str)
-	from pprint import pprint
-	pprint(d)
-	import json
-	assert False
+    brenda = BrendaParser()
+    ec = "1.1.1.2"
+    ec_str = brenda.ec_text[ec]
+    d = brenda.parse_info_dict(ec_str)
+    from pprint import pprint
+    pprint(d)
+    import json
+    assert False
+'''
